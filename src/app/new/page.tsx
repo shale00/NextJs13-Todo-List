580055b8a502247e4bd0a535973e9a4bc582e124
Scott Hale <sth0067@gmail.com>
2023-10-03 14:16:53 -0500

    removed Header component and updated home page and new page

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,10 +1,13 @@
-import Header from '@/components/Header'
-import React from 'react'
+import Link from 'next/link';
+import React from 'react';
 
 const NewPage = () => {
   return (
-    <Header />
-  )
-}
+    <header className='flex justify-between items-center mb-4'>
+      <h1 className='text-2xl'>New</h1>
+      <Link href={'/'}>Home</Link>
+    </header>
+  );
+};
 
-export default NewPage
+export default NewPage;
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -10,10 +10,7 @@ const TodoItem = ({ id, title, complete }: TodoItemProps) => {
   return (
     <li className='flex gap-1 items-center'>
       <input id={id} type='checkbox' className='cursor-pointer peer' />
-      <label
-        htmlFor={id}
-        className='cursor-pointer peer-checked:line-through peer-checked:text-slate-500'
-      >
+      <label htmlFor={id} className='peer-checked:line-through'>
         {title}
       </label>
     </li>
