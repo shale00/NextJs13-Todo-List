4ba5251fe096dd766f99f0e024a642ccef9fc12a
Scott Hale <sth0067@gmail.com>
2023-10-03 02:43:12 -0500

    added styling to todo list item when checked

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -10,7 +10,10 @@ const TodoItem = ({ id, title, complete }: TodoItemProps) => {
   return (
     <li className='flex gap-1 items-center'>
       <input id={id} type='checkbox' className='cursor-pointer peer' />
-      <label htmlFor={id} className='peer-checked:line-through'>
+      <label
+        htmlFor={id}
+        className='cursor-pointer peer-checked:line-through peer-checked:text-slate-500'
+      >
         {title}
       </label>
     </li>
